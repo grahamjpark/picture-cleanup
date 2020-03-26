8fd96ccb3d3b94704dd5c82e64e8decd814f037f
Graham Park <code@grahamjpark.com>
2020-03-25 22:08:12 -0400

    fixing for espanso

--- daily-template.py
+++ daily-template.py
@@ -1,24 +1,21 @@
 import os
 import requests
 import datetime
-import pyperclip
+from keys import TODOIST_KEY
+# import pyperclip
 
 today = datetime.date.today()
-
 output = today.strftime('%B %-d, %Y\n')
 
 url = "https://api.todoist.com/rest/v1/tasks"
-
 querystring = {"filter":"(today | overdue)"}
-
 payload = ""
-headers = {"authorization": f"Bearer {os.environ['TODOIST_KEY']}"}
+headers = {"authorization": f"Bearer {TODOIST_KEY}"}
 
 response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
 
 for task in response.json():
     output += " [ ] {}\n".format(task.get('content'))
 
-# print(output)
-
-pyperclip.copy(output)
+print(output)
+# pyperclip.copy(output)
